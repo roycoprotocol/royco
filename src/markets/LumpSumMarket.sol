6087c927d29a4d849860075c1849d29b69d14b21
JetJadeja <jetjadeja@gmail.com>
2024-07-28 17:02:44 +0300

    Implement OrderCreationLogic

--- src/markets/LumpSumMarket.sol
+++ src/markets/LumpSumMarket.sol
@@ -2,10 +2,15 @@
 pragma solidity ^0.8.0;
 
 import {Market, MarketType} from "./interfaces/Market.sol";
+
 import {OrderFactory} from "./interfaces/OrderFactory.sol";
+import {Order} from "../Order.sol";
 
 import {ERC20} from "../../lib/solmate/src/tokens/ERC20.sol";
 
+/// @title Lump Sum Market
+/// @author Royco
+/// @notice Market contract for lump sum rewards.
 contract LumpSumMarket is Market, OrderFactory {
     /*//////////////////////////////////////////////////////////////
                             INITIALIZE
@@ -33,5 +38,62 @@ contract LumpSumMarket is Market, OrderFactory {
         orderImplementation = _orderImplementation;
         tokens = _tokens;
         weirollCommands = _weirollCommands;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                         ORDER CREATION LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice New order creation event.
+    event OrderCreated(Order.Side indexed side, address indexed order, address indexed owner);
+
+    /// @notice Deploy a new Action Order.
+    /// @param tokenAmounts Array of token amounts requested as rewards.
+    /// @param weirollState Array of weiroll state to passed into the market action.
+    function createActionOrder(uint256[] calldata tokenAmounts, bytes[] calldata weirollState) external {
+        // Deploy the clone.
+        Order clone = Order(deployClone(msg.sender, address(this), uint256(Order.Side.ActionOrder)));
+
+        // Initialize the order.
+        clone.initialize(tokenAmounts, weirollState);
+
+        // Emit the event.
+        emit OrderCreated(Order.Side.ActionOrder, address(clone), msg.sender);
+    }
+
+    /// @notice Deploy a new Reward Order.
+    /// @param tokenAmounts Array of token amounts to be utilized as rewards.
+    /// @param weirollState Array of requested weiroll state to be passed into the market action.
+    function createRewardOrder(uint256[] calldata tokenAmounts, bytes[] calldata weirollState) external {
+        // Deploy the clone.
+        Order clone = Order(deployClone(msg.sender, address(this), uint256(Order.Side.RewardOrder)));
+
+        // Initialize the order.
+        clone.initialize(tokenAmounts, weirollState);
+
+        // Emit the event.
+        emit OrderCreated(Order.Side.RewardOrder, address(clone), msg.sender);
+
+        // Transfer the tokens to the order which will hold them until the order is executed.
+        for (uint256 i = 0; i < tokens.length; i++) {
+            tokens[i].transferFrom(msg.sender, address(clone), tokenAmounts[i]);
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        ORDER CANCELATION LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Cancel an order.
+    /// @param order Address of the order to be cancelled.
+    function cancelOrder(address order) external {
+        // Cast the order to the Order contract.
+        Order castedOrder = Order(order);
+
+        // Ensure that the order is owned by the caller.
+        require(castedOrder.owner() == msg.sender, "LumpSumMarket: caller is not the owner of the order");
+
+        // Cancel the order.
+        castedOrder.cancel(tokens);
     }
 }
