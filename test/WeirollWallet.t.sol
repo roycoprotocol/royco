a3f20b42168e02aec36d0677d3eb7ea22897ca4c
JetJadeja <jetjadeja@gmail.com>
2024-07-28 11:51:54 +0300

    comment out weiroll wallet tests

--- test/WeirollWallet.t.sol
+++ test/WeirollWallet.t.sol
@@ -1,37 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-// Smart wallet contracts
-import { WeirollWallet } from "src/WeirollWallet.sol";
-import { WalletFactory } from "src/WalletFactory.sol";
+// // Smart wallet contracts
+// import { WeirollWallet } from "src/WeirollWallet.sol";
+// import { WalletFactory } from "src/WalletFactory.sol";
 
-// Testing contracts
-import { DSTestPlus } from "solmate/test/utils/DSTestPlus.sol";
+// // Testing contracts
+// import { DSTestPlus } from "solmate/test/utils/DSTestPlus.sol";
 
-contract WeirollWalletTest is DSTestPlus {
-    ExampleFactory public factory;
+// contract WeirollWalletTest is DSTestPlus {
+//     ExampleFactory public factory;
 
-    function setUp() public {
-        factory = new ExampleFactory();
-    }
+//     function setUp() public {
+//         factory = new ExampleFactory();
+//     }
 
-    function testWalletCreation() public {
-        factory.createWallet(address(this), address(0));
-        assert(address(factory).code.length > 0);
-    }
+//     function testWalletCreation() public {
+//         factory.createWallet(address(this), address(0));
+//         assert(address(factory).code.length > 0);
+//     }
 
-    function testWalletVariablesSet() public {
-        WeirollWallet wallet = WeirollWallet(factory.createWallet(address(0x01), address(this)));
-        assertEq(wallet.getOwner(), address(0x01));
-        assertEq(wallet.getOrderbook(), address(this));
-    }
-}
+//     function testWalletVariablesSet() public {
+//         WeirollWallet wallet = WeirollWallet(factory.createWallet(address(0x01), address(this)));
+//         assertEq(wallet.getOwner(), address(0x01));
+//         assertEq(wallet.getOrderbook(), address(this));
+//     }
+// }
 
-// Example Factory for deploying wallet implementation clowns
-contract ExampleFactory is WalletFactory {
-    constructor() WalletFactory(address(new WeirollWallet())) { }
+// // Example Factory for deploying wallet implementation clowns
+// contract ExampleFactory is WalletFactory {
+//     constructor() WalletFactory(address(new WeirollWallet())) { }
 
-    function createWallet(address owner, address orderbook) public returns (address) {
-        return address(deployClone(owner, orderbook));
-    }
-}
+//     function createWallet(address owner, address orderbook) public returns (address) {
+//         return address(deployClone(owner, orderbook));
+//     }
+// }
