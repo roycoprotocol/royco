faf73f3334cb59253d65b0d7f478697d2caba6cc
ControlCplusControlV <randomlikeabledude@gmail.com>
2024-07-30 14:45:26 -0700

    compiles now, laid out general structure

--- src/LPOrder.sol
+++ src/LPOrder.sol
@@ -1,19 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import {VM} from "lib/weiroll/contracts/VM.sol";
-import {Clone} from "lib/clones-with-immutable-args/src/Clone.sol";
-import {Owned} from "lib/solmate/src/auth/Owned.sol";
-import {ERC20} from "lib/solmate/src/tokens/ERC20.sol";
+import { VM } from "lib/weiroll/contracts/VM.sol";
+import { Clone } from "lib/clones-with-immutable-args/src/Clone.sol";
+import { Owned } from "lib/solmate/src/auth/Owned.sol";
+import { SafeTransferLib } from "lib/solmate/src/utils/SafeTransferLib.sol";
+import { ERC20 } from "lib/solmate/src/tokens/ERC20.sol";
 
 /// @title OrderFactory
 /// @author Royco
 /// @notice LPOrder implementation contract.
 ///   Implements a simple LP order to supply an asset for a given action
 contract LPOrder is Clone, VM {
+    using SafeTransferLib for ERC20;
     /*//////////////////////////////////////////////////////////////
                                MODIFIERS
     //////////////////////////////////////////////////////////////*/
+
     error NotOwner();
     error NotMarket();
     error WalletLocked();
@@ -28,7 +31,7 @@ contract LPOrder is Clone, VM {
 
     /// @notice Only the orderbook contract can call the function
     modifier onlyOrderbook() {
-        if (msg.sender != market()) {
+        if (msg.sender != orderbook()) {
             revert NotMarket();
         }
         _;
@@ -45,50 +48,58 @@ contract LPOrder is Clone, VM {
     /*//////////////////////////////////////////////////////////////
                             INITIALIZATION
     //////////////////////////////////////////////////////////////*/
-    ERC20 immutable public depositToken;
+    /// @dev Whether or not this order has been executed
     bool public executed;
 
-    function fundSweepToNewOrder(address newAddress, uint256 amountToSweep) onlyOrderbook external {
-      _depositToken.transfer(newAddress, amountToSweep);
+    /// @param markets The markets for which this order is valid
+    function initialize(uint256[] calldata markets) external onlyOrderbook {
+        ERC20 _depositToken = depositToken();
+        _depositToken.transferFrom(owner(), address(this), amount());
+
+        /// Allowlist all markets
+        for (uint256 i; i < markets.length; i++) {
+            supportedMarkets[i] = true;
+        }
     }
 
-    function initialize(
-      uint256[] calldata markets
-    ) onlyOrderbook external {
-      ERC20 _depositToken = depositToken();
-      _depositToken.transferFrom(owner(), address(this), amount());
-
-      /// Allowlist all markets
-      for(uint256 i; i < markets.length; i++) {
-        supportedMarkets[i] = true;
-      }
+    /// @param newAddress The new address to sweep funds to
+    /// @param amountToSweep The amount of tokens to transfer to the new address
+    function fundSweepToNewOrder(address newAddress, uint256 amountToSweep) external onlyOrderbook {
+        ERC20 _depositToken = depositToken();
+        _depositToken.safeTransfer(newAddress, amountToSweep);
     }
 
     /*//////////////////////////////////////////////////////////////
                                STATE VARIABLES
     //////////////////////////////////////////////////////////////*/
-    
+
     /// @notice The address of the order creator (owner)
     function owner() public pure returns (address) {
         return _getArgAddress(0);
     }
-    
+
+    /// @notice The address of the orderbook exchange contract
     function orderbook() public pure returns (address) {
-      return _getArgAddress(60);
+        return _getArgAddress(20);
     }
 
+    /// @notice The deposit token being LP'ed
     function depositToken() public pure returns (ERC20) {
-      return ERC20(_getArgAddress(20));
+        return ERC20(_getArgAddress(40));
     }
 
+    /// @notice The amount of tokens to be LP'ed
     function amount() public pure returns (uint256) {
-      return _getArgUint256(40);
-    } 
+        return _getArgUint256(60);
+    }
 
-    mapping(uint256 marketId => bool) public supportedMarkets;   
+    /// @notice The max duration to lock for
+    function maxDuration() public pure returns (uint256) {
+        return _getArgUint256(92);
+    }
 
-    address incentiveProvider;
-    addres 
+    /// @notice Whether or not a market is supported by this order
+    mapping(uint256 marketId => bool) public supportedMarkets;
     /*//////////////////////////////////////////////////////////////
                                LOCKING LOGIC
     //////////////////////////////////////////////////////////////*/
@@ -110,7 +121,7 @@ contract LPOrder is Clone, VM {
 
     /// @notice Cancel the order.
     /// @dev Only the owner of the order can cancel it.
-    function cancel(ERC20[] calldata tokens) public onlyOrderbook {
+    function cancel() public onlyOrderbook {
         // Mark the order as cancelled.
         cancelled = true;
     }
@@ -118,18 +129,11 @@ contract LPOrder is Clone, VM {
     /*//////////////////////////////////////////////////////////////
                                EXECUTION LOGIC
     //////////////////////////////////////////////////////////////*/
-    error AlreadyRan()
-
     /// @notice Execute the Weiroll VM with the given commands.
     /// @param commands The commands to be executed by the Weiroll VM.
     /// @dev No state parameter is necessary because the proposed state is stored in the contract.
-    function executeWeiroll(
-        bytes32[] calldata commands,
-        bytes[] calldata state
-    ) public payable onlyOrderbook notLocked returns (bytes[] memory) {
-        if (executed) {
-          revert AlreadyRan();
-        }
+    function executeWeiroll(bytes32[] calldata commands, bytes[] calldata state) public payable onlyOrderbook notLocked returns (bytes[] memory) {
+        require(!executed, "Royco: AlreadyRan");
 
         executed = true;
         // Execute the Weiroll VM.
@@ -137,13 +141,14 @@ contract LPOrder is Clone, VM {
     }
 
     /// @notice Execute a generic call to another contract.
-    function execute(
-        address to,
-        uint256 value,
-        bytes memory data
-    ) public onlyOwner notLocked returns (bytes memory) {
+    /// @param to The address to call
+    /// @param value The ether value of the execution
+    /// @param data The data to pass along with the call
+    function execute(address to, uint256 value, bytes memory data) public onlyOwner notLocked returns (bytes memory) {
+        // Prevent people from approving w/e then rugging during vesting
+        require(executed, "Royco: Order unfilled");
         // Execute the call.
-        (bool success, bytes memory result) = to.call{value: value}(data);
+        (bool success, bytes memory result) = to.call{ value: value }(data);
         if (!success) {
             revert("Generic execute proxy failed"); //TODO: Better revert message (stringify result?)
         }
--- src/Orderbook.sol
+++ src/Orderbook.sol
@@ -1,147 +1,157 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-// Interfaces 
+// Interfaces
 import { ERC20 } from "lib/solmate/src/tokens/ERC20.sol";
 
-// Libraries 
+// Libraries
 import { ECDSA } from "lib/solady/src/utils/ECDSA.sol";
 import { SafeTransferLib } from "lib/solmate/src/utils/SafeTransferLib.sol";
 import { FixedPointMathLib } from "lib/solady/src/utils/FixedPointMathLib.sol";
+import { ClonesWithImmutableArgs } from "lib/clones-with-immutable-args/src/ClonesWithImmutableArgs.sol";
 
-// Contracts 
+// Contracts
 import { LPOrder } from "src/LPOrder.sol";
 
-contract LSUnlockedOrderbook {
-  /*//////////////////////////////////////////////////////////////
-                             INTERFACE
-  //////////////////////////////////////////////////////////////*/
-  event OrderSubmitted {
-    address order, 
-    address creator,
-    uint256[] markets
-  }
+contract Orderbook {
+    using ClonesWithImmutableArgs for address;
+    using SafeTransferLib for ERC20;
 
-  /*//////////////////////////////////////////////////////////////
-                              STORAGE
-  //////////////////////////////////////////////////////////////*/
-  struct IPOrder {
-    address sender;
-    uint96 duration;
-    uint128 amount;
-    uint128 incentiveAmount;
-    uint128 marketId;
-    uint128 nonce;
-  }
+    address public immutable ORDER_IMPLEMENTATION;
 
-  uint256 public maxIPOrderId;
-  mapping(uint256 IPOrderId => IPOrder) public IpOrders; 
-  
-  uint256 public maxLPOrderId;
-  mapping(uint256 LPOrderId => LPOrder) public LpOrders; 
-
-  enum MarketType {
-    FL_Vesting, // Front-loaded Vesting
-    BL_Vesting, // Back-loaded Vesting 
-    Streaming,
-    Forfeitable_Vested
-  }
-  
-  struct Market {
-    ERC20 depositToken;
-    ERC20 primaryRewardToken;
-    MarketType _type;
-    Aloha enter;
-    Aloha exit;
-  }
-  
-  /// @dev Its for both enter/exit so the named fit
-  struct Aloha {
-    bytes32[] weirollCommands,
-    bytes[] weirollState
-  }
-
-  uint256 public maxMarketId;
-  mapping(uint256 marketId => Market _market) public markets;
-
-  mapping(ERC20 rewardToken => mapping(LPOrder order => uint256 rewardsOwed)) public OrderRewardsOwed;
-
-  /*//////////////////////////////////////////////////////////////
-                           MARKET ACTIONS
-  //////////////////////////////////////////////////////////////*/
-  function createMarket(
-    ERC20 _depositToken,
-    ERC20 _primaryRewardToken,
-    MarketType marketType, 
-    Aloha enterMarket,
-    Aloha exitMarekt
-  ) public returns (uint256 marketId) {
-    marketId = maxMarketId++;
-
-    markets[marketId] = Market({
-      depositToken: _depositToken,
-      primaryRewardToken: _primaryRewardToken,
-      _type: marketType,
-      enter: enterMarket,
-      exit: enterMarket
-    });
-  }
-
-  /*//////////////////////////////////////////////////////////////
-                               ORDERS
-  //////////////////////////////////////////////////////////////*/
-  function createLPOrder(ERC20 depositToken, uint256 tokenAmount, uint96 maxDuration, uint256[] calldata markets) external returns (address clone, uint256 orderId) {
-    LPOrder clone = LPOrder(deployClone(msg.sender, address(depositToken), tokenAmount, address(this)));
-    
-    orderId = maxLPOrderId++
-    clone.initialize(markets);
-
-    LpOrders[orderId] = clone;
-
-    emit OrderSubmitted(address(clone), msg.sender, markets);
-  }
-
-  function createIPOrder(uint96 _duration, uint128 _amount, uint128 _incentiveAmount, uint128 _marketId) external returns (uint256 IPOrderId){
-    Market memory _market = markets[_marketId];
-    _market.primaryRewardToken.safeTransferFrom(msg.sender, address(this), _incentiveAmount);
-
-    Order memory order = Order({
-      sender: msg.sender,
-      duration: _duration,
-      amount: _amount,
-      incentiveAmount: _incentiveAmount,
-      marketId: _marketId
-    });
-
-    IPOrderId = maxIPOrderId++;
-    IpOrders[IPOrderId] = order;
-  }
-
-  function matchOrders(uint256 IPOrderId, uint256 LPOrderId) public {
-    Order memory IpOrder = IpOrders[IPOrderId];
-    LPOrder _LpOrder = LpOrders[LPOrderId];
-
-    Market memory _market = markets[IpOrder.marketId];
-
-    // Enter the script
-    _LpOrder.executeWeiroll(_market.weirollCommands, _market.weirollState);
-
-    // Lock the wallet for the time if neccessary 
-    if (_market._type != MarketType.Streaming) {
-      _LpOrder.lockWallet(block.timestamp + IpOrder.duration);
+    constructor(address orderbook_impl) {
+        ORDER_IMPLEMENTATION = orderbook_impl;
     }
 
-    OrderRewardsOwed[_market.primaryRewardToken][_LpOrder] = IpOrder.incentiveAmount;
-  }
+    /*//////////////////////////////////////////////////////////////
+                             INTERFACE
+    //////////////////////////////////////////////////////////////*/
+    event OrderSubmitted(address order, address creator, uint256[] markets);
 
-  function validateOrder(Order memory IpOrder, LPOrder lpOrder) view public {
-    require(lpOrder.supportedMarkets(IpOrder.marketId), "Royco: Market Mismatch");
-    require(lpOrder.duration() > IpOrder.duration, "Royco: Duration Mismatch");
-    
-  }
+    /*//////////////////////////////////////////////////////////////
+                              STORAGE
+    //////////////////////////////////////////////////////////////*/
+    struct IPOrder {
+        address sender;
+        uint96 duration;
+        uint128 amount;
+        uint128 incentiveAmount;
+        uint128 marketId;
+    }
 
-  function claimRewards(LPOrder order) public {
-     
-  }
+    uint256 public maxIPOrderId;
+    mapping(uint256 IPOrderId => IPOrder) public IpOrders;
 
+    uint256 public maxLPOrderId;
+    mapping(uint256 LPOrderId => LPOrder) public LpOrders;
+
+    enum MarketType {
+        FL_Vesting, // Front-loaded Vesting
+        BL_Vesting, // Back-loaded Vesting
+        Streaming,
+        Forfeitable_Vested
+    }
+
+    struct Market {
+        ERC20 depositToken;
+        ERC20 primaryRewardToken;
+        MarketType _type;
+        Aloha enter;
+        Aloha exit;
+    }
+
+    /// @dev Its for both enter/exit so the named fit
+    struct Aloha {
+        bytes32[] weirollCommands;
+        bytes[] weirollState;
+    }
+
+    uint256 public maxMarketId;
+    mapping(uint256 marketId => Market _market) public markets;
+
+    mapping(ERC20 rewardToken => mapping(LPOrder order => uint256 rewardsOwed)) public OrderRewardsOwed;
+
+    /*//////////////////////////////////////////////////////////////
+                           MARKET ACTIONS
+    //////////////////////////////////////////////////////////////*/
+    function createMarket(
+        ERC20 _depositToken,
+        ERC20 _primaryRewardToken,
+        MarketType marketType,
+        Aloha calldata enterMarket,
+        Aloha calldata exitMarket
+    )
+        public
+        returns (uint256 marketId)
+    {
+        marketId = maxMarketId++;
+
+        markets[marketId] =
+            Market({ depositToken: _depositToken, primaryRewardToken: _primaryRewardToken, _type: marketType, enter: enterMarket, exit: enterMarket });
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                               ORDERS
+    //////////////////////////////////////////////////////////////*/
+    function createLPOrder(
+        ERC20 depositToken,
+        uint256 tokenAmount,
+        uint96 maxDuration,
+        uint256[] calldata allowedMarkets
+    )
+        external
+        returns (LPOrder clone, uint256 orderId)
+    {
+        clone = LPOrder(ORDER_IMPLEMENTATION.clone(abi.encode(msg.sender, address(depositToken), tokenAmount, address(this), maxDuration)));
+
+        orderId = maxLPOrderId++;
+        clone.initialize(allowedMarkets);
+
+        LpOrders[orderId] = clone;
+
+        emit OrderSubmitted(address(clone), msg.sender, allowedMarkets);
+    }
+
+    function createIPOrder(uint96 _duration, uint128 _amount, uint128 _incentiveAmount, uint128 _marketId) external returns (uint256 IPOrderId) {
+        Market memory _market = markets[_marketId];
+        _market.primaryRewardToken.safeTransferFrom(msg.sender, address(this), _incentiveAmount);
+
+        IPOrder memory order = IPOrder({ sender: msg.sender, duration: _duration, amount: _amount, incentiveAmount: _incentiveAmount, marketId: _marketId });
+
+        IPOrderId = maxIPOrderId++;
+        IpOrders[IPOrderId] = order;
+    }
+
+    function matchOrders(uint256 IPOrderId, uint256 LPOrderId) public {
+        IPOrder memory IpOrder = IpOrders[IPOrderId];
+        LPOrder _LpOrder = LpOrders[LPOrderId];
+
+        Market memory _market = markets[IpOrder.marketId];
+
+        // Enter the script
+        _LpOrder.executeWeiroll(_market.enter.weirollCommands, _market.enter.weirollState);
+
+        // Lock the wallet for the time if neccessary
+        if (_market._type != MarketType.Streaming) {
+            _LpOrder.lockWallet(block.timestamp + IpOrder.duration);
+        }
+
+        OrderRewardsOwed[_market.primaryRewardToken][_LpOrder] = IpOrder.incentiveAmount;
+    }
+
+    function validateOrder(IPOrder memory IpOrder, LPOrder lpOrder) public view {
+        require(lpOrder.supportedMarkets(IpOrder.marketId), "Royco: Market Mismatch");
+        require(lpOrder.maxDuration() > IpOrder.duration, "Royco: Duration Mismatch");
+    }
+
+    /// @param token The Token to Claim Rewards In
+    /// @param order The (fufilled) Order to claim rewards for
+    function claimRewards(ERC20 token, LPOrder order) public {
+        uint256 owed = OrderRewardsOwed[token][order];
+        if (order.lockedUntil() > block.timestamp) {
+            return;
+        }
+
+        token.safeTransfer(order.owner(), owed);
+    }
 }
