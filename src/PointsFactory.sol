50a0f2682c256ba98931a6dd91d34a84177e0c95
ControlCplusControlV <randomlikeabledude@gmail.com>
2024-08-31 18:06:27 -0400

    [Points.sol] Added in _decimals naming consistency

--- src/PointsFactory.sol
+++ src/PointsFactory.sol
@@ -17,17 +17,17 @@ contract PointsFactory {
 
     /// @param _name The name for the new points program
     /// @param _symbol The symbol for the new points program
-    /// @param decimals The amount of decimals for 1 Point
+    /// @param _decimals The amount of decimals for 1 Point
     /// @param _allowedVault The ERC4626i Incentivized Vault
     /// @param _orderbook The RecipeOrderbook for Weiroll Rewards Programs
     function createPointsProgram(
         string memory _name,
         string memory _symbol,
-        uint256 decimals,
+        uint256 _decimals,
         ERC4626i _allowedVault,
         RecipeOrderbook _orderbook
     ) external returns (Points points) {
-        points = new Points(_name, _symbol, decimals, _allowedVault, _orderbook);
+        points = new Points(_name, _symbol, _decimals, _allowedVault, _orderbook);
         pointsPrograms.push(address(points));
         isPointsProgram[address(points)] = true;
 
