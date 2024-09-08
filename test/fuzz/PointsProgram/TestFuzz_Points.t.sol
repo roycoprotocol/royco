2f7360877f4ec157c45ef1ea3f5d05c0dd6c935a
DhruvP <dhruvpareek883@gmail.com>
2024-09-08 15:20:00 -0700

    Fix import statements

--- test/fuzz/PointsProgram/TestFuzz_Points.t.sol
+++ test/fuzz/PointsProgram/TestFuzz_Points.t.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.0;
 
 import "../../../src/Points.sol";
 import "../../../src/PointsFactory.sol";
-import { ERC4626i, ERC4626 } from "../../../src/ERC4626i.sol";
+import { ERC4626i } from "../../../src/ERC4626i.sol";
+import { ERC4626 } from "../../../lib/solmate/src/tokens/ERC4626.sol";
 import { Ownable } from "lib/solady/src/auth/Ownable.sol";
 import { RoycoTestBase } from "../../utils/RoycoTestBase.sol";
 
