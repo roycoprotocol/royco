9c98376022cf9820fe6a4decfd9c6bd993c5d20c
DhruvP <dhruvpareek883@gmail.com>
2024-09-09 22:05:13 -0700

    Fixed testOrderCOnditionsNotMet

--- test/VaultOrderbook.t.sol
+++ test/VaultOrderbook.t.sol
@@ -400,7 +400,7 @@ contract VaultOrderbookTest is Test {
        // Mock the previewRateAfterDeposit function
        vm.mockCall(
            address(targetVault),
-           abi.encodeWithSelector(ERC4626i.previewRateAfterDeposit.selector, uint256(0), uint256(100 * 1e18)),
+           abi.encodeWithSelector(ERC4626i.previewRateAfterDeposit.selector, address(baseToken), uint256(100 * 1e18)),
            abi.encode(2e18)
            );
        // Allocate the order
