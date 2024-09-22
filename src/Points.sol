d8d439771d3f258613796bcdca4b988ddff324f7
ControlCplusControlV <randomlikeabledude@gmail.com>
2024-09-22 16:36:27 -0400

    [#26 + #25] Unneeded imports

--- src/Points.sol
+++ src/Points.sol
@@ -1,11 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import { ERC4626i } from "src/ERC4626i.sol";
 import { RecipeOrderbook } from "src/RecipeOrderbook.sol";
-
 import { Ownable } from "lib/solady/src/auth/Ownable.sol";
-import { ERC20 } from "lib/solmate/src/tokens/ERC20.sol";
 
 /// @title Points
 /// @author CopyPaste, corddry
